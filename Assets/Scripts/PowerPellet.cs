23ae1736daf08e183f7ab20e2905b8b14e13563a
K K Kavin <kavinkathiravan6787@gmail.com>
2026-02-17 22:44:41 +0530

    Auto-commit: Assets/Scenes/maze.unity, Assets/Scripts/PowerPellet.cs, Assets/XR/XRGeneralSettings.asset at 2/17/2026, 10:44:41 PM

--- Assets/Scripts/PowerPellet.cs
+++ Assets/Scripts/PowerPellet.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class PowerPellet : MonoBehaviour
 {
+    public float rotSpeed;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = 
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddTorque(0,0,rotSpeed);
     }
 }
