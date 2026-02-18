01dae7e87094cdd0b54f77eb25c373e2f531ec3b
K K Kavin <kavinkathiravan6787@gmail.com>
2026-02-18 16:30:10 +0530

    Auto-commit: Assets/Scripts/PowerPellet.cs at 2/18/2026, 4:30:10 PM

--- Assets/Scripts/PowerPellet.cs
+++ Assets/Scripts/PowerPellet.cs
@@ -8,12 +8,12 @@ public class PowerPellet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddTorque(0,0,rotSpeed);
+        transform.rotate(0,0,rotSpeed);
     }
 }
