dfdc115655cf9a5107c2ddd25b53f93b19451d51
K K Kavin <kavinkathiravan6787@gmail.com>
2026-02-18 16:27:22 +0530

    Auto-commit: Assets/Scripts/PowerPellet.cs at 2/18/2026, 4:27:22 PM

--- Assets/Scripts/PowerPellet.cs
+++ Assets/Scripts/PowerPellet.cs
@@ -5,11 +5,10 @@ using UnityEngine;
 public class PowerPellet : MonoBehaviour
 {
     public float rotSpeed;
-    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
