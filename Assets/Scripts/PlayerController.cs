59e326fa1c0cc0fb1027e83d2a7310c6910151aa
K K Kavin <kavinkathiravan6787@gmail.com>
2026-02-18 11:04:43 +0530

    Auto-commit: Assets/Scripts/PlayerController.cs at 2/18/2026, 11:04:42 AM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject GameOverManager;
+    public GameObject GameManager;
     public float speed = 10f;
     public Rigidbody rb;
     void Update()
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour
         }
         if (collision.CompareTag("Ghost"))
         {
-            GameOverManager.ShowGameOver();
+            GameManager.ShowGameOver();
         }
     }
 }
