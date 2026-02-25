e125842949371a34465a7573293999e6fd6b6a56
unknown <23csd3@CSELAB.IN>
2026-02-25 16:57:57 +0530

    Auto-commit: Assets/Scenes/basketball.unity, Assets/VuforiaLicense.cs, Assets/VuforiaLicense.cs.meta at 2/25/2026, 4:57:57 PM

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVVPb05FYi8vLy8vQUFBQm1aY2xCcVA3WlV6K25MazBkN1BqcHJVRDFhZDMrTzFGYTV3OGxremMxV3VFdnlZN0EreksxanZ2TWpHWW9PVWFrSVJvd21UV0RXTHY4eGZicXdYV0VpTjdRck1TMXVzQXhOMVpka21IKzBibERCWnRWY2dnUU5UeHpsbnR2WHNpek01WllYd2Y3ZlNtRThQTDY3R05ZOTB4Y0V4bE1IeXFKTjkrL0xlU0VVYnM4SEZXNTFsQ1EydERDTG9YWWtpbGJRc0pHWU5zbXVQeUNpNGxPYXRVWkpqaDRDUjdqVlIzWWJKeG0rTDMrWkFReWU4Wk44b1lRZkMxSmFxUHBCN2tIL2pyc2tFUGIvNkYwTlI4alZkZ0FxVU9YTElCUyt5YzR6R0pTL0l6c1hLN0xhblRhTGwxYmMwamh6TWU5bHBrQ1Y2UGx2V2VZOVZUZnc2L21oRjcwUWZnMlltbXhvc0k4dFlZYk55YkhHbFc=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
