c3ec3de2cb64d5f75d362e74a7b1e2fc3475c584
K K Kavin <kavinkathiravan6787@gmail.com>
2026-02-26 16:04:21 +0530

    Auto-commit: "Assets/My Assets/Scripts/Archery/Arrow.cs" at 2/26/2026, 4:04:21 PM

--- Assets/My Assets/Scripts/Archery/Arrow.cs
+++ Assets/My Assets/Scripts/Archery/Arrow.cs
@@ -9,7 +9,7 @@ public class Arrow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(arrow,)
     }
 
     // Update is called once per frame
