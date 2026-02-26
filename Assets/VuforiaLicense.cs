a1b8ed5de7a2601630e318a1489a60a9478d59f0
K K Kavin <kavinkathiravan6787@gmail.com>
2026-02-26 16:14:02 +0530

    Auto-commit: Assets/Resources/VuforiaConfiguration.asset, Assets/Resources/alpha.shader, Assets/Resources/alpha.shader.meta, Assets/VuforiaLicense.cs, Assets/VuforiaLicense.cs.meta, Assets/XR/Temp.meta, Assets/XR/UserSimulationSettings/Resources/XRSimulationPreferences.asset -> Assets/XR/Temp/XRSimulationPreferences.asset, Assets/XR/UserSimulationSettings/Resources/XRSimulationPreferences.asset.meta -> Assets/XR/Temp/XRSimulationPreferences.asset.meta, Assets/XR/Resources/XRSimulationRuntimeSettings.asset -> Assets/XR/Temp/XRSimulationRuntimeSettings.asset, Assets/XR/Resources/XRSimulationRuntimeSettings.asset.meta -> Assets/XR/Temp/XRSimulationRuntimeSettings.asset.meta, ProjectSettings/ProjectSettings.asset at 2/26/2026, 4:14:01 PM

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QWNWRFAzUC8vLy8vQUFBQm1lRGY2UXdkdjBIbHBwUTU3VUdSRU54RmJyUFg4TWEwQXJERFZ5TnJZWjJFVXpGTFRqTjBiZlgvS3BkdG9MQzlpRG16MTFGVks1VEgxd0kweTBEMzFrMVZRN0hsdlZrZ052dm1XS0R5TjlScmRFNTVXZUtNeUg3aWNnQk9LdjhTZ0NzbFZsd05FOWxqdGZHMDg1NTlYeCtpbTUyVHlidldZc0RtaEZaT2hlYzI5dWhZV1NxQUxJYk5TM0pnWFlXb0JjYThzdkxHL05CanV4THNvVUNjWmFvWUVxVlRLZytRTHRQRkQzc3RYVFBsbWMwa2tNeFU4YVJib3g4TjlHcXBEMm03SUNLTTEyZndYWFBYRmVQdDNCLzRSek9CZTV3MkZFVWVJQ29WS0hOVjI4NE9Oc0dmU2cxTDhsRDMwWlZtRHc5ZmcySmV6RXJUM2Z5Kzh5empvQ0ZON1VyTTloaGVGTWdXVVRjNFRkaWw=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
