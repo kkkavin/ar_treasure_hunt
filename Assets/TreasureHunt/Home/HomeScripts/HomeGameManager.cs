82d503ddc47989f484c6df8fdc42ed0bbfb935a1
K K Kavin <kavinkathiravan6787@gmail.com>
2026-03-05 08:21:26 +0530

    Auto-commit: Assets/TreasureHunt/Home/HomeScripts/HomeGameManager.cs at 3/5/2026, 8:21:26 AM

--- Assets/TreasureHunt/Home/HomeScripts/HomeGameManager.cs
+++ Assets/TreasureHunt/Home/HomeScripts/HomeGameManager.cs
@@ -6,6 +6,38 @@ using UnityEngine.SceneManagement;
 public class HomeGameManager : MonoBehaviour
 {
     public GameObject homepage;
+    public static GameManager Instance;
+
+    // This is the "Registry" slot where the Worker will plug itself in
+    [Header("Registry")]
+    public GameObject homepageObject; 
+
+    void Awake()
+    {
+        // Singleton logic: If a boss already exists, destroy the new one
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // This makes it persistent
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // This function can now be called from ANY scene to activate the object
+    public void ToggleHomepage(bool status)
+    {
+        if (homepageObject != null)
+        {
+            homepageObject.SetActive(status);
+        }
+        else
+        {
+            Debug.LogWarning("Manager: No object is registered in the 'homepageObject' slot!");
+        }
+    }
     public void LoadScene(string sceneName)
     {
         homepage.SetActive(false);
