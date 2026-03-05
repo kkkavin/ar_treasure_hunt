66201d028350cdeda201875a8ec3fa250f487e9b
K K Kavin <kavinkathiravan6787@gmail.com>
2026-03-05 08:43:51 +0530

    Auto-commit: Assets/TreasureHunt/Maze/mScripts/MazeGameManager.cs at 3/5/2026, 8:43:51 AM

--- Assets/TreasureHunt/Maze/mScripts/MazeGameManager.cs
+++ Assets/TreasureHunt/Maze/mScripts/MazeGameManager.cs
@@ -95,6 +95,6 @@ public class MazeGameManager : MonoBehaviour
         Time.timeScale = 1f; // unpause before going home
         HomeGameManager.Instance.ToggleHomepage(true);
         SceneManager.SetActiveScene(SceneManager.GetSceneByName("Home"));
-        SceneManager.UnloadSceneAsync("archery");
+        SceneManager.UnloadSceneAsync("maze");
     }
 }
