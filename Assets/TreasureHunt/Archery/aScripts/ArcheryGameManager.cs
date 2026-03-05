e58ae90277c6b15d38b47b6e5e47eb6d521fa432
K K Kavin <kavinkathiravan6787@gmail.com>
2026-03-05 08:41:59 +0530

    Auto-commit: Assets/TreasureHunt/Archery/aScripts/ArcheryGameManager.cs at 3/5/2026, 8:41:58 AM

--- Assets/TreasureHunt/Archery/aScripts/ArcheryGameManager.cs
+++ Assets/TreasureHunt/Archery/aScripts/ArcheryGameManager.cs
@@ -162,5 +162,7 @@ public class ArcheryGameManager : MonoBehaviour
 
     public void goHome()
     {
+        Time.timeScale = 1f; // unpause before going home
+        HomeGameManager.Instance.ToggleHomepage(true);
     }
 }
